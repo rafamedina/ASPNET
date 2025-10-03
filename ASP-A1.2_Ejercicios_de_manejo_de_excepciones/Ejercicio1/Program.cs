367b47609ee7e78e37aaafeec6fdf3b47b82d35a
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-10-03 12:59:32 +0200

    Ejercicios viernes 3-10

---
+++ ASP-A1.2_Ejercicios_de_manejo_de_excepciones/Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+class Ejercicio1
+{
+    public static void Main(string[] args)
+    {
+        convertirNumero();
+
+    }
+
+    public static int convertirNumero()
+    {
+        while (true)
+        {
+            Console.WriteLine("Dame un numero");
+            string PreNumero = Console.ReadLine();
+
+            if (int.TryParse(PreNumero, out int resultado) && resultado > 0)
+            {
+                Console.WriteLine("Numero aceptado: " + resultado);
+                return resultado;
+            }
+            else
+            {
+                Console.WriteLine("Numero no válido");
+            }
+        }
+
+    }
+
+}
---
+++ ASP-A1.2_Ejercicios_de_manejo_de_excepciones/Ejercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio2
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, int> diccionario = new Dictionary<string, int>();
+        diccionario.Add("Iker", 15);
+        diccionario.Add("Rafa", 25);
+
+        if (diccionario.TryGetValue("Rafa", out int edad))
+        {
+            Console.WriteLine(edad); 
+        }
+        else
+        {
+            Console.WriteLine("Persona no encontrada.");
+        }
+    }
+}
