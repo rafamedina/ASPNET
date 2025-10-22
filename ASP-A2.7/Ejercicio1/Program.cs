85e6b31d9a7b2aa72b955e42f555c2ea1b1efdc2
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-10-22 12:20:56 +0200

    22/10/2025

---
+++ ASP-A2.7/Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Ejercicio1
+{
+    
+    public static void Main(string[] args)
+    {
+        var  numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    var numerosMayores5 = from n in numeros where n > 5 select n;
+        Console.WriteLine(string.Join(",", numerosMayores5));
+        var numerosMayores5lambda = numeros.Where(n => n > 5);
+        Console.WriteLine(string.Join(",", numerosMayores5lambda)); ;
+
+
+        var numerosPares = from n in numeros where n % 2 == 0 select n;
+        Console.WriteLine(string.Join(",", numerosPares));
+
+        var numerosPareslambda = numeros.Where(n => n % 2 == 0);
+        Console.WriteLine(string.Join(",", numerosPareslambda));
+
+        var numerosx3 = from n in numeros where n% 3 == 0 select n;
+        Console.WriteLine(string.Join(",", numerosx3));
+
+        var numerosx3lambda = numeros.Where(n => n % 3 == 0);
+        Console.WriteLine(string.Join(",", numerosx3lambda));
+    }
+}
---
+++ ASP-A2.7/Ejercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Ejercicio2
+{
+
+    public static void Main(string[] args)
+    {
+        var palabras = new List<string> { "casa", "coche", "árbol", "mesa", "silla" };
+        var mas4letras = from n in palabras where n.Length > 4 select n;
+        Console.WriteLine(string.Join(",", mas4letras));
+        var mas4letraslambda = palabras.Where(p => p.Length > 4);
+        Console.WriteLine(string.Join(",", mas4letraslambda));
+
+
+        var mayusculas = from n in palabras select n.ToUpper();
+        Console.WriteLine(string.Join(",", mayusculas));
+
+        var mayusculaslambda = palabras.Select(p => p.ToUpper());
+        Console.WriteLine(string.Join(",", mayusculaslambda));
+
+        var anonimolambda = palabras.Select(p => new { palabra = p, numero = p.Length });
+        Console.WriteLine(string.Join(",", anonimolambda));
+
+        var anonimo = from p in palabras
+                      select new { palabra = p, numero = p.Length };
+        Console.WriteLine(string.Join(",", anonimo));
+    }
+
+
+}
