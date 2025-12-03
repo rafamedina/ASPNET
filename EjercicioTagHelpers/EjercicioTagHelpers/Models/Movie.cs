a90aea37131399a28f0562ca84b3cf83799ae524
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-12-03 10:57:15 +0100

    Taghelpers

---
+++ EjercicioTagHelpers/EjercicioTagHelpers/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations; // <--- NECESARIO PARA [Required]
+
+namespace EjercicioTagHelpers.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
